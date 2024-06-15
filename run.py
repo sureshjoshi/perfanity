ed9f5496e4aeaf21a578b02bad19e60c6e8cc32c
sj <suresh@vicarasolutions.com>
2024-06-15 14:04:48 -0400

    Added some more logging throughout - to see where delays happen

--- run.py
+++ run.py
@@ -56,22 +56,15 @@ class TestMetrics:
 
     duration_seconds: float
     cache_status: CacheStatus
-    
 
-def run_goal(target: str):
-    start = time.time()
-    subprocess.run(["time", "pants", "run", target])
-    end = time.time()
-    duration = end - start
-    print(f"  Running took {duration} seconds")
-    return RunMetrics(duration_seconds=duration, startup_time_seconds=0)
 
 def package_target(target: str) -> PackageMetrics:
+    print(f"--> {target}: Starting Package")
     start = time.time()
     subprocess.run(["time", "pants", "package", target])
     end = time.time()
     duration = end - start
-    print(f"  Packaging {target} took {duration} seconds")
+    print(f"--> {target}: Package took {duration} seconds")
 
     split = target.split(":")
     target_dir = (DIST_DIR / split[0] / split[1]).with_suffix(".pex")
@@ -82,18 +75,29 @@ def package_target(target: str) -> PackageMetrics:
         output_number_of_files=len(file_sizes)
     )
 
+def run_goal(target: str):
+    print(f"--> {target}: Starting Run")
+    start = time.time()
+    subprocess.run(["time", "pants", "run", target])
+    end = time.time()
+    duration = end - start
+    print(f"--> {target}: Run took {duration} seconds")
+    return RunMetrics(duration_seconds=duration, startup_time_seconds=0)
+
 def test_target(target: str):
     """TODO: This is just structural right now, the tests need to be parametrized first"""
+    print(f"--> {target}: Starting Test")
     start = time.time()
     subprocess.run(["time", "pants", "test", "simple:"])
     end = time.time()
     duration = end - start
-    print(f"  Testing took {duration} seconds")
+    print(f"--> {target}: Test took {duration} seconds")
     return TestMetrics(duration_seconds=duration, cache_status=TestMetrics.CacheStatus.NA)
 
 
 def run_simple() -> pd.DataFrame:
     def _bust_cache():
+        print("--> Busting sources cache")
         content = Path("./simple/main.py").read_text().splitlines()
         content[0] = f"# CACHE_BUSTING={time.time()}"
         Path("./simple/main.py").write_text("\n".join(content))
@@ -110,22 +114,41 @@ def run_simple() -> pd.DataFrame:
 
     for execution_mode in ["venv", "zipapp"]:
         for layout in ["loose", "packed", "zipapp"]:
-            print(f"  Packaging pex with execution_mode={execution_mode} and layout={layout}")
             target = f"simple:bin@execution_mode={execution_mode},layout={layout}"
+            
+            print(f"***** {target}: Starting sequence with execution_mode={execution_mode} and layout={layout} *****")
+            start = time.time()
+            
+            print(f"==> {target}: Starting Pex sequence")
+            start_pex = time.time()
             
             _run_sequence(df, Step.CLEAN, target)
             _run_sequence(df, Step.NOOP, target)
             _bust_cache()
             _run_sequence(df, Step.INCREMENTAL, target)
 
+            end_pex = time.time()
+            duration_pex = end_pex - start_pex
+            print(f"==> {target}: Pex sequence took {duration_pex} seconds")
+
             if subprocess.run(["which", "docker"]).returncode == 0:
-                print("  Packaging pex with docker")
+                print(f"==> {target}: Starting Docker sequence")
+                start_docker = time.time()
+
                 docker_target = target.replace("@", "_").replace("=", "_").replace(",", "_").replace("bin", "img")
                 # TODO: Do something to actually "Clean" this
                 _run_sequence(df, Step.CLEAN, docker_target)
                 _run_sequence(df, Step.NOOP, docker_target)
                 _bust_cache()
                 _run_sequence(df, Step.INCREMENTAL, docker_target)
+
+                end_docker = time.time()
+                duration_docker = end_docker - start_docker
+                print(f"==> {target}: Docker sequence took {duration_docker} seconds")
+
+            end = time.time()
+            duration = end - start
+            print(f"***** {target}: Sequence took {duration} seconds *****")
 
     return df
 
@@ -138,7 +161,10 @@ if __name__ == "__main__":
     # index = pd.Index([])
     df = pd.DataFrame(columns=mi)
     
+    start = time.time()
     simple_results = run_simple()
+    end = time.time()
 
     df = pd.concat([df, simple_results], axis=0)
+    print(f"Total script time: {end - start}")
     print(df)
